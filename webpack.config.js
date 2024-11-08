946a7e6f994914e19838ecde384cb5a8149cd51f
SaiSakethGK <saisaketh.gootykase@unh.edu>
2024-11-08 00:53:32 -0500

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
 const path = require('path');
 const copyPlugin = require('copy-webpack-plugin');
 const htmlPlugin = require('html-webpack-plugin');
+const { get } = require('http');
 
 module.exports = {
     mode: 'development',
     devtool: 'cheap-module-source-map',
     entry: {
         popup: path.resolve('src/popup/popup.tsx'),
+        options: path.resolve('src/options/options.tsx'),
     },
     module: {
         rules: [{
             use: 'ts-loader',
             test: /\.tsx?$/,
             exclude: /node_modules/,
-        }]
+        },
+        {
+            use: ['style-loader', 'css-loader'],
+            test: /\.css$/i,
+        },
+        {
+            type: 'asset/resource',
+            test: /\.(jpg|png|jpe?g|gif|woff|woff2|eot|tff|svg)$/,
+        }
+    ]
     },
     plugins: [
         new copyPlugin({
             patterns: [
                 {
-                    from: path.resolve('src/manifest.json'),
+                    from: path.resolve('src/static'),
                     to: path.resolve('dist')
                 }
             ]
         }),
-        new htmlPlugin({
-            title: 'React Extension',
-            filename: 'popup.html',
-            chunks: ['popup'],
-        })
+        ...getHtmlPlugins(['popup', 'options'])
     ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
@@ -36,5 +43,18 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve('dist')
+    },
+    optimization:{
+        splitChunks: {
+            chunks: 'all'
+        }
     }
+}
+
+function getHtmlPlugins(chunks){
+    return chunks.map(chunk => new htmlPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+    }))
 }
